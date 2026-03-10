1748806e893c546f2c2f785ce5322ca263c89885
alesikivan <obcofficiall@gmail.com>
2026-03-10 12:02:02 +0300

    added google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import './globals.css';
 import { Providers } from './providers';
 
@@ -136,6 +137,18 @@ export default function RootLayout({
         />
       </head>
       <body className="font-sans antialiased">
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-D5YTGTTGEX"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-D5YTGTTGEX');
+          `}
+        </Script>
         <Providers>{children}</Providers>
       </body>
     </html>
